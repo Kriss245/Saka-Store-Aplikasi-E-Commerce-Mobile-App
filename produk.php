0a06a1a5e8aab2ff2f6ed74c715798f947ed2053
Kristian Abel <43513503+Kriss245@users.noreply.github.com>
2025-05-31 00:18:40 +0700

    Tambah File
    Berisi koneksi produk ke database MySQL

---
+++ produk.php
@@ -0,0 +1,33 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbsakastore";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, nama_barang, harga_barang, jumlah_barang, imageUrl, deskripsi FROM produk";
+$result = $conn->query($sql);
+
+$products = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $products[] = $row;
+    }
+}
+
+echo json_encode($products);
+
+$conn->close();
+?>
